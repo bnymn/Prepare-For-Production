ddeb69626aedef84dc5f00386a022621e728f42c
Bunyamin <inanbunyamin90@gmail.com>
2017-02-11 08:50:04 +0300

    Fix

--- build.sh
+++ build.sh
@@ -1,3 +1,22 @@
-# First of all update the server
-apt update && apt upgrade
+# Install rbenv system wide
+./rbenv-install-system-wide.sh
 
+# Install dependencies for Ruby
+apt install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs
+
+# Install bundler
+gem install bundler
+
+# Install NodeJS
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+apt install -y nodejs
+
+# Install rails
+gem install rails -v 4.2.7.1
+rbenv rehash
+
+# Install MySQL
+apt install mysql-server mysql-client libmysqlclient-dev
+
+# Install nginx
+apt install nginx
---
+++ rbenv-install-system-wide.sh
@@ -0,0 +1,34 @@
+# Author: jnx (https://gist.github.com/jnx)
+# Link: https://gist.github.com/jnx/1256593
+
+# Update, upgrade and install development tools:
+apt-get update
+apt-get -y upgrade
+apt-get -y install build-essential
+apt-get -y install git-core
+
+# Install rbenv
+git clone git://github.com/sstephenson/rbenv.git /usr/local/rbenv
+
+# Add rbenv to the path:
+echo '# rbenv setup' > /etc/profile.d/rbenv.sh
+echo 'export RBENV_ROOT=/usr/local/rbenv' >> /etc/profile.d/rbenv.sh
+echo 'export PATH="$RBENV_ROOT/bin:$PATH"' >> /etc/profile.d/rbenv.sh
+echo 'eval "$(rbenv init -)"' >> /etc/profile.d/rbenv.sh
+
+chmod +x /etc/profile.d/rbenv.sh
+source /etc/profile.d/rbenv.sh
+
+# Install ruby-build:
+pushd /tmp
+  git clone git://github.com/sstephenson/ruby-build.git
+  cd ruby-build
+  ./install.sh
+popd
+
+# Install Ruby 1.9.2-p290:
+rbenv install 2.3.1
+rbenv global 2.3.1
+
+# Rehash:
+rbenv rehash
